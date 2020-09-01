54e4b86f95ed5ad3fd6f77516dbdb029ace91c32
harry7naga <h4ry7naga@gmail.com>
2020-09-01 22:52:04 +0700

    update Tugas 7

--- Tugas-7/tugas7.js
+++ Tugas-7/tugas7.js
@@ -52,3 +52,39 @@ console.log(kodok.jump)
 // Soal 2
 console.log(" ")
 console.log("----- Soal 2 -----")
+class Clock {
+  constructor({template}){
+      this.template = template
+  }
+    render = () => {
+    var date = new Date();
+
+    var hours = date.getHours();
+    if (hours < 10) hours = '0' + hours;
+
+    var mins = date.getMinutes();
+    if (mins < 10) mins = '0' + mins;
+
+    var secs = date.getSeconds();
+    if (secs < 10) secs = '0' + secs;
+
+    var output = this.template
+      .replace('h', hours)
+      .replace('m', mins)
+      .replace('s', secs);
+
+    console.log(output);
+  }
+
+    stop() {
+    clearInterval(this.timer);
+    };
+
+    start() {
+    this.render();
+    this.timer = setInterval(this.render, 1000);
+    };
+}
+
+    var clock = new Clock({template: 'h:m:s'});
+      clock.start();
